d2d0ecdf975b30843da4ccc5a8a5e9bee4f97b42
farhanalih <farhanalih96@gmail.com>
2021-03-05 13:50:24 +0700

    Bill Topup gan

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/home'
 import Cart from './pages/cart'
+import billTopup from './pages/billTopup'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/cart" component={Cart} />
+          <Route path="/billTopup" component={billTopup}/>
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,7 +8,7 @@ export default function Navbar({ fixed }) {
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <a className="leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-gray-800">
-            <span className="text-xl font-bold">E-Commerce CODE.ID</span>
+            <span className="text-xl font-bold">E-Commerce</span>
           </a>
           <button
             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
