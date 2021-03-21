d60629961b1ddb0013ea801241b37ec23e0aab8f
agas <irfanrsbi@gmail.com>
2021-03-21 18:15:00 +0700

    merge_ahmad

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -61,6 +61,13 @@ export default function Navbar({ fixed }) {
     <nav className="fixed z-50 w-full bg-primary top-0 flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg shadow-lg">
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+        <button
+            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <i className="fas fa-bars" />
+          </button>
           <a className="leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-gray-800" href='/'> 
             <span className="text-xl font-bold">E-Commerce</span>
           </a>
